63a518a6e0fd1d0d1b7680297ffe819f82b4414b
gjf <GuoJianFeng3092@163.com>
2019-05-28 22:51:17 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/yangming/git/FirstClass.java
@@ -0,0 +1,20 @@
+package com.yangming.git;
+
+/**
+ * @author gjf
+ * @date 2019/5/28 22:44
+ * @description TODO
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
